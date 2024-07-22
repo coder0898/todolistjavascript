38d0c904350951e7167b52896093e37dc32795af
Pratik Bakshi <bakshi.pratikcool@gmail.com>
2024-07-22 16:58:03 +0530

    display functionality added

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const taskInput = document.getElementById("task-input");
 const addTask = document.getElementById("add-task");
 
+const ListTask = document.getElementById("todo-list");
+
+let TaskList = [];
+
 function AddTaskInput(inputData) {
   const id = new Date();
   const taskData = {
@@ -9,8 +13,24 @@ function AddTaskInput(inputData) {
     task: inputData,
   };
 
-  console.log(taskData);
+  TaskList.push(taskData);
+
+  //   console.log(taskData);
   console.log("data added successfully");
+}
+
+function DisplayTask() {
+  TaskList.forEach((item) => {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = `${item.task}`;
+    const deleteButton = document.createElement("button");
+    deleteButton.id = `delete-task`;
+    deleteButton.innerText = "Delete";
+    li.appendChild(span);
+    li.appendChild(deleteButton);
+    ListTask.appendChild(li);
+  });
 }
 
 addTask.addEventListener("click", () => {
@@ -20,5 +40,13 @@ addTask.addEventListener("click", () => {
     alert("Please enter valid input data");
   } else {
     AddTaskInput(inputData);
+    taskInput.value = "";
   }
+  DisplayTask();
 });
+
+if (ListTask !== "") {
+  DisplayTask();
+} else {
+  ListTask.innerHTML = `<p>No data to show</p>`;
+}
